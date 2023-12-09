42fd8a4d507d1bedfa01e504661dff18e025b049
Nesb01t <nesb01t@163.com>
2023-12-09 18:28:24 +0800

    import element

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import App from './App.vue'
 import router from './router';
 
-import { IonicVue } from '@ionic/vue';
+import {IonicVue} from '@ionic/vue';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/vue/css/core.css';
@@ -23,10 +23,15 @@ import '@ionic/vue/css/display.css';
 /* Theme variables */
 import './theme/variables.css';
 
+/* Element plus */
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css'
+
 const app = createApp(App)
   .use(IonicVue)
-  .use(router);
-  
+  .use(router)
+  .use(ElementPlus);
+
 router.isReady().then(() => {
   app.mount('#app');
 });
