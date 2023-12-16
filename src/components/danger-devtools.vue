7486e075f79ac26e53779df77259031115901f09
Nesb01t <nesb01t@163.com>
2023-12-16 10:59:50 +0800

    update devtools always show + router link utils

--- src/components/danger-devtools.vue
+++ src/components/danger-devtools.vue
@@ -13,13 +13,30 @@ const { style } = useDraggable(el, {
 
 // need render?
 const show = computed(() => {
-  return devStore.logs.length || devStore.elements.length;
+  // when debug, always show
+  return true;
+  // return devStore.logs.length || devStore.elements.length;
 });
 </script>
 
 <template>
   <div v-if="show" ref="el" style="position: fixed" :style="style">
     <el-card>
+      <el-text>Easy devtools</el-text>
+      <br />
+      <!-- Router -->
+      <ion-button
+        class="danger-router-button"
+        href="home"
+        size="small"
+        style="margin-bottom: 5px"
+        >go home
+      </ion-button>
+      <br />
+      <ion-button class="danger-router-button" size="small" href="detail"
+        >go detail
+      </ion-button>
+
       <!-- logs -->
       <el-text v-if="devStore.logs.length" size="large">Logs</el-text>
       <br />
@@ -34,4 +51,9 @@ const show = computed(() => {
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+ion-button {
+  --background: linear-gradient(45deg, #e66465, #9198e5) !important;
+  --color: #fff;
+}
+</style>
