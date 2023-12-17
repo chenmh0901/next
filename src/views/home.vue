114aa8b0baf570124c054289071fa6dcc9bbbb23
Nesb01t <nesb01t@163.com>
2023-12-17 10:53:16 +0800

    revert commit signin

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,27 @@
 <template>
   <ion-page>
-    <ion-header :translucent="true">
+    <ion-header :translucent="true" style="box-shadow: none">
       <ion-toolbar>
-        <ion-title>这个标题应该是不需要的，除非多端适配有效果</ion-title>
+        <ion-title style="margin-left: 50%">芸社</ion-title>
+        <ion-buttons slot="end">
+          <ion-button>
+            <ion-icon :icon="addCircleOutline" size="large"></ion-icon>
+          </ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
 
     <ion-content :fullscreen="true">
-      {{ users }}
+      <ion-searchbar
+        placeholder="搜索"
+        style="--box-shadow: none; border-bottom: grey solid 1px"
+      ></ion-searchbar>
     </ion-content>
+    <ion-footer>
+      <ion-toolbar>
+        <ion-title style="margin-left: 40%">Footer</ion-title>
+      </ion-toolbar>
+    </ion-footer>
   </ion-page>
 </template>
 
@@ -16,10 +29,14 @@
 import {
   IonContent,
   IonHeader,
+  IonFooter,
   IonPage,
   IonTitle,
-  IonToolbar
+  IonToolbar,
+  IonIcon,
+  IonSearchbar
 } from '@ionic/vue';
+import { addCircleOutline } from 'ionicons/icons';
 import 'swiper/css';
 import { ref } from 'vue';
 import { User } from '@/types/user';
