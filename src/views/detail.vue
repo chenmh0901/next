250d0f6530b774f51d76078b97dffc06ec01802e
Nesb01t <nesb01t@163.com>
2023-12-17 12:52:57 +0800

    fix page to detail when reg

--- src/views/detail.vue
+++ src/views/detail.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { IonContent, IonText } from '@ionic/vue';
+import { IonCard, IonContent, IonText } from '@ionic/vue';
 import DetailForm from '@/components/detail/detail-form.vue';
 </script>
 
@@ -7,7 +7,9 @@ import DetailForm from '@/components/detail/detail-form.vue';
   <ion-content>
     <ion-text>还差最后一步！</ion-text>
     <ion-text>您还需要完善一些信息来加入芸馆</ion-text>
-    <detail-form></detail-form>
+    <ion-card style="height: 60%; width: 60%; margin: 20%">
+      <detail-form style="height: 100%; width: 100%"></detail-form>
+    </ion-card>
   </ion-content>
 </template>
 
