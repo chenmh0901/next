cb85fa36625c89f0b861d4bf4c45e032e5d52ae4
amumu <1210204146@zust.edu.cn>
2024-02-01 19:22:16 +0800

    update profile form + add edit

--- src/views/profile/profile.vue
+++ src/views/profile/profile.vue
@@ -3,16 +3,24 @@
         <ion-header>
             <ion-toolbar>
                 <ion-title>Profile</ion-title>
+                <ion-buttons slot="end">
+                    <ion-button>
+                        <ion-icon :icon="settingsOutline" size="large"></ion-icon>
+                    </ion-button>
+                </ion-buttons>
             </ion-toolbar>
         </ion-header>
         <ion-content>
             <div class="profile-avatar">
-                <div style="width: 150px;height: 150px;">
-                    <img src="../../../resources/icon.png" alt="">
-                </div>
+                <ion-avatar style="width: 150px;height: 150px;--border-radius:4px;" v-if="!edit">
+                    <img src="../../../resources/icon.png" alt="avatar loading">
+                </ion-avatar>
+                <ion-avatar style="width: 150px;height: 150px;--border-radius:4px;" v-else>
+                    <img src="../../../resources/splash.png" alt="avatar loading">
+                </ion-avatar>
             </div>
             <div class="profile-info">
-                <profile-form :user="user" />
+                <profile-form :user="user" :edit="edit" :change-mode="() => { edit = !edit; }" />
             </div>
         </ion-content>
         <ion-footer>
@@ -24,23 +32,27 @@
 </template>
 
 <script setup lang="ts">
-import { IonContent, IonPage, IonFooter, IonHeader } from '@ionic/vue';
+import { IonContent, IonPage, IonFooter, IonHeader, IonAvatar } from '@ionic/vue';
+import { settingsOutline } from 'ionicons/icons';
 import ProfileForm from './components/profile-form.vue';
 import { User } from '@/types/user';
-const user: User = {
-    id: 123,
-    no: "string",
-    name: "string",
-    admin: false,
-    email: "string",
-    wechat: "string",
-    QQ: "string",
-    phone: "string",
-    class: "string",
-    room: "string",
-    birthPlace: "string",
-    resume: "string"
-};
+import { ref } from 'vue';
+
+const edit = ref(false);
+const user = ref<User>({
+    id: 1,
+    no: '',
+    name: '',
+    admin: true,
+    email: '',
+    wechat: '',
+    QQ: '',
+    phone: '',
+    class: '',
+    room: '',
+    birthPlace: '',
+    resume: '',
+} as User);
 </script>
 
 <style coped>
@@ -55,6 +67,5 @@ const user: User = {
 .profile-info {
     display: flex;
     justify-content: center;
-    height: 600px;
 }
 </style>
