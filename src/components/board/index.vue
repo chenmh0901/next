93775c1d0986c28f9206d6bd950dff57af421838
nesb01t <nesb01t@163.com>
2024-02-14 04:56:04 +0800

    refactor style of board

--- src/components/board/index.vue
+++ src/components/board/index.vue
@@ -13,7 +13,8 @@ const msgs = computed(() => {
       date: '2024/2/5',
       type: '通知',
       title: '各位同学，紧急通知！紧急通知！',
-      content: '我可以发广告吗？\n' +
+      content:
+        '我可以发广告吗？\n' +
         '后端实习薪资 400～500/天.\n' +
         '还有 产品，前端，运营，基架\n' +
         '工作时间弹性，免费三餐下午茶，21 点半后下班免费打车\n' +
@@ -30,48 +31,48 @@ const msgDetail = ref();
 
 <template>
   <div class="home-users flex flex-wrap">
-    <div
+    <ion-card
       v-for="msg in msgs"
-      :id="msg.id"
       :key="msg.id"
-      class="'w-full p-1'"
+      @click="
+        () => {
+          msgDetail = msg.content;
+          show = true;
+        }
+      "
     >
-      <ion-card
-        class="flex flex-row"
-        @click="
-          () => {
-            msgDetail = msg.content;
-            show = true;
-          }
-        "
-      >
-        <div class="w-1/3 h-1/3 p-5">
-          <img
-            alt="Grapefruit slice atop a pile of other slices"
-            src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
-            style="
-              border-radius: 20px;
-              width: 100%;
-              height: 100%;
-              box-shadow:
-                rgba(0, 0, 0, 0.2) 0 4px 1px -2px,
-                rgba(0, 0, 0, 0.14) 0 3px 2px 0,
-                rgba(0, 0, 0, 0.12) 0 2px 5px 0;
-            "
-          />
-        </div>
-        <div class="flex flex-col justify-center p-5">
-          <ion-card-title class="mb-2">{{ msg.type }}</ion-card-title>
-          <ion-card-subtitle style="font-size: 1rem"> {{ msg.name }}</ion-card-subtitle>
-          <ion-card-content class="p-0">{{ msg.title }}</ion-card-content>
-          <ion-card-subtitle class="ml-20"> 发布时间：{{ msg.date }}</ion-card-subtitle>
-        </div>
-      </ion-card>
-    </div>
+      <div class="w-1/3 h-1/3 p-5">
+        <img
+          alt="Grapefruit slice atop a pile of other slices"
+          src="https://interactive-examples.mdn.mozilla.net/media/cc0-images/grapefruit-slice-332-332.jpg"
+          style="
+            border-radius: 20px;
+            width: 100%;
+            height: 100%;
+            box-shadow:
+              rgba(0, 0, 0, 0.2) 0 4px 1px -2px,
+              rgba(0, 0, 0, 0.14) 0 3px 2px 0,
+              rgba(0, 0, 0, 0.12) 0 2px 5px 0;
+          "
+        />
+      </div>
+      <div class="flex flex-col justify-center p-5">
+        <ion-card-title class="mb-2">{{ msg.type }}</ion-card-title>
+        <ion-card-subtitle style="font-size: 1rem">
+          {{ msg.name }}
+        </ion-card-subtitle>
+        <ion-card-content class="p-0">{{ msg.title }}</ion-card-content>
+        <ion-card-subtitle class="ml-20">
+          发布时间：{{ msg.date }}
+        </ion-card-subtitle>
+      </div>
+    </ion-card>
   </div>
-  <dialog-msg-card :close="()=>{
-    show=false
-  }" :msg="msgDetail" :show="show"></dialog-msg-card>
+  <dialog-msg-card
+    :close="() => (show = false)"
+    :msg="msgDetail"
+    :show="show"
+  ></dialog-msg-card>
 </template>
 
 <style scoped></style>
