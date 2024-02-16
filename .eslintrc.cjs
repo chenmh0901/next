c62c5f324c56cd4f4ad187aa48c34cfd23329003
nesb01t <nesb01t@163.com>
2024-02-17 00:00:40 +0800

    update eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,28 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
+    browser: true
   },
   extends: [
+    'plugin:vue/recommended',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/typescript/recommended'
+    '@vue/typescript/recommended',
+    'plugin:vue/recommended'
   ],
   parserOptions: {
+    parser: "@babel/eslint-parser",
+    requireConfigFile: false,
     ecmaVersion: 2020
   },
   rules: {
+    'prettier/prettier': 'error',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'vue/no-deprecated-slot-attribute': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-deprecated-slot-attribute': 'off',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
     'vue/multi-word-component-names': 'off'
   }
 };
