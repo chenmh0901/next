dfdc4dfdbdb3b16c0cbbd61145b0d213bac26436
chenmh0901 <1210204146@zust.edu.cn>
2024-02-17 20:16:03 +0800

    update user-list usehttp + update user type

--- src/components/user-list/index.vue
+++ src/components/user-list/index.vue
@@ -2,29 +2,11 @@
 import { IonCard, IonIcon, IonButton } from '@ionic/vue';
 import { copyOutline } from 'ionicons/icons';
 import DialogUserDetail from '@/components/user-list/components/dialog-user-detail.vue';
-import { computed, ref } from 'vue';
+import { computed, onBeforeMount, ref } from 'vue';
 import { useEasyToggle } from '@/composables/use-easy-toggle';
+import { IHttpOptions, useHttp } from '@/utils/http';
+import { User } from '@/types/user';
 
-const users = computed(() => {
-  const user = [];
-  for (let i = 0; i < 3; i++) {
-    user.push({
-      id: i + 1,
-      no: '1210204146',
-      name: '陈明浩',
-      admin: true,
-      email: 'string',
-      wechat: 'string',
-      QQ: 'string',
-      phone: 'string',
-      class: '数媒212',
-      room: 'string',
-      birthPlace: 'string',
-      resume: 'string'
-    });
-  }
-  return user;
-});
 const show = ref(false);
 const userDetail = ref();
 
@@ -35,6 +17,22 @@ enum ShowMode {
 
 //  show mode
 const { val, toggle } = useEasyToggle([ShowMode.COL, ShowMode.ROW]);
+
+const users = ref<User[]>([]);
+const fetchUsers = async () => {
+  const options: IHttpOptions<any> = {
+    path: 'user/',
+    method: 'get'
+  };
+  const res = await useHttp(options);
+  res.data.map((item: User) => {
+    users.value.push(item);
+  });
+};
+
+onBeforeMount(() => {
+  fetchUsers();
+});
 </script>
 
 <template>
@@ -71,7 +69,7 @@ const { val, toggle } = useEasyToggle([ShowMode.COL, ShowMode.ROW]);
         <div class="flex flex-col justify-center p-5">
           <ion-card-title class="mb-2">{{ user.name }}</ion-card-title>
           <ion-card-subtitle> {{ user.no }}</ion-card-subtitle>
-          <ion-card-subtitle> {{ user.class }}</ion-card-subtitle>
+          <ion-card-subtitle> {{ user.className }}</ion-card-subtitle>
         </div>
       </IonCard>
     </div>
