450d22d4cc2fae257dd9a23d336bfecba91995c6
chenmh0901 <1210204146@zust.edu.cn>
2024-02-24 14:48:32 +0800

    update user store + use camera photo

--- src/utils/camara.ts
+++ src/utils/camara.ts
@@ -1,14 +1,18 @@
 import { Camera, CameraResultType } from '@capacitor/camera';
+import { UserPhoto } from '@/types/user';
 
 export const useCamera = () => {
-  const open = () => {
+  const photo: UserPhoto = { filepath: '' };
+  const take = () => {
     Camera.getPhoto({
       quality: 90,
       allowEditing: true,
       resultType: CameraResultType.Uri
-    }).then((r) => {
-      console.log(r.webPath);
+    }).then((p) => {
+      if (p.webPath) {
+        photo.filepath = p.webPath;
+      }
     });
   };
-  return { open };
+  return { take, photo };
 };
