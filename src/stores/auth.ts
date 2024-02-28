7f3db027a510deb77ef51e518291f6f3d80d880d
chenmh0901 <1210204146@zust.edu.cn>
2024-02-28 16:58:08 +0800

    update expiretime in auth token

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,17 +1,35 @@
 import { Storage } from '@ionic/storage';
 
+interface IToken {
+  val: string;
+  time: number;
+}
+
 export const useAuthStore = () => {
   const storage = new Storage();
 
   const setToken = async (t: string) => {
     const s = await storage.create();
-    await s.set('token', t);
+    const token: IToken = { val: t, time: new Date().getTime() };
+    await s.set('token', token);
   };
 
   const getToken = async () => {
     const s = await storage.create();
-
-    return s.get('token');
+    const token = await s.get('token');
+    const expireTime = 7 * 24 * 60 * 60 * 1000;
+    if (checkTokenExpire(token, expireTime)) {
+      return token.val;
+    } else {
+      await removeToken();
+      return null;
+    }
+  };
+  const checkTokenExpire = (token: IToken, expireTime: number) => {
+    const currentTime = new Date().getTime();
+    if (token) {
+      return currentTime - token.time < expireTime;
+    }
   };
 
   const removeToken = async () => {
