d6e7b6da50f43ea8d5cc5b03b5ab3f8f1fd5e2f7
nesb01t <nesb01t@163.com>
2024-02-28 23:04:28 +0800

    fix(plain) chinese char

--- src/components/user-list/user-detail.vue
+++ src/components/user-list/user-detail.vue
@@ -24,44 +24,44 @@ const user = computed(() => {
   <div v-if="user" class="user__detail">
     <IonItem>
       <Icon icon="material-symbols:person-outline" />
-      姓名：{{ user?.name }}
+      姓名: {{ user?.name }}
     </IonItem>
     <IonItem>
       <Icon icon="ic:outline-class" />
-      班级：{{ user?.className }}
+      班级: {{ user?.className }}
     </IonItem>
     <IonItem>
       <Icon icon="cil:room" />
-      寝室：{{ user?.roomName }}
+      寝室: {{ user?.roomName }}
     </IonItem>
     <IonItem>
       <Icon icon="tdesign:address-book" />
-      籍贯：{{ user?.birthPlace }}
+      籍贯: {{ user?.birthPlace }}
     </IonItem>
     <IonItem>
       <Icon icon="tabler:phone-call" />
-      手机号：{{ user?.phone }}
+      手机号: {{ user?.phone }}
     </IonItem>
     <IonItem>
       <Icon icon="mingcute:wechat-line" />
-      微信：{{ user?.wechat }}
+      微信: {{ user?.wechat }}
     </IonItem>
     <IonItem>
       <Icon icon="ri:qq-line" />
-      QQ：{{ user?.qq }}
+      QQ: {{ user?.qq }}
     </IonItem>
     <IonItem>
       <Icon icon="mdi:comment-outline" />
-      简介：{{ user?.selfResume }}
+      简介: {{ user?.selfResume }}
     </IonItem>
     <template v-if="isAdmin">
       <IonItem>
         <Icon icon="mdi:address-marker-outline" />
-        地址：{{ user?.address }}
+        地址: {{ user?.address }}
       </IonItem>
       <IonItem>
         <Icon icon="icon-park-outline:comment" />
-        留言：{{ user?.adminResume }}
+        留言: {{ user?.adminResume }}
       </IonItem>
     </template>
   </div>
