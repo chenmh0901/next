08aaed1a12fc8d5caff1eb76a6e5f53ffb5f895f
amumu <1210204146@zust.edu.cn>
2024-03-12 10:32:21 +0800

    update alert options

--- src/components/logout-btn/index.vue
+++ src/components/logout-btn/index.vue
@@ -3,7 +3,11 @@ import { Icon } from '@iconify/vue';
 import { useAlert } from '@/composables/use-alert';
 import { logout } from '@/utils/logout';
 
-const { userChoice: show, alert } = useAlert('提示', '确认退出登录吗？');
+const { userChoice: show, alert } = useAlert({
+  header: '提示',
+  message: '确认退出登录吗？',
+  btnstext: ['取消', '继续']
+});
 const onClick = async () => {
   await alert();
   if (show.value) {
